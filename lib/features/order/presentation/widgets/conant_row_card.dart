04a301ecf27649a5d361aafcdf15776fa5eef796
Rahaf Alhaj <2141410@std.hu.edu.jo>
2025-12-24 18:39:58 +0300

    update help center ,seller , notification ,onboardig images & icons assets

--- lib/features/order/presentation/widgets/conant_row_card.dart
+++ lib/features/order/presentation/widgets/conant_row_card.dart
@@ -3,24 +3,30 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../../core/theming/styles.dart';
 
 class ContactRowWidget extends StatelessWidget {
-  final String imagePath;
+  final IconData? icon;
+  final String? imagePath;
   final String text;
 
   const ContactRowWidget({
     super.key,
-    required this.imagePath,
+    this.icon,
+    this.imagePath,
     required this.text,
   });
 
   @override
   Widget build(BuildContext context) {
     // شرط اللون للواتساب
-    final bool isWhatsApp = imagePath.contains("whatsapp");
+    final bool isWhatsApp = text.toLowerCase().contains("whatsapp");
     final Color textColor = isWhatsApp ? const Color(0xff18D92F) : Colors.black;
 
     return Row(
       children: [
-        Image.asset(imagePath, width: 18.w, height: 18.w, fit: BoxFit.contain),
+        if (icon != null) ...[
+          Icon(icon, size: 24.sp, color: Colors.black),
+        ] else if (imagePath != null) ...[
+          Image.asset(imagePath!, width: 18.w, height: 18.w, fit: BoxFit.contain),
+        ],
         SizedBox(width: 12.w),
         Expanded(
           child: Text(
